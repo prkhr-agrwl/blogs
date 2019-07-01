2953c6287631b2ec8b64fc252b507bdffe756742
prkhr-agrwl <aprakhar51@gmai.com>
2019-07-02 02:56:43 +0530

    v2 memoized soln to redundant get requests

--- src/actions/index.js
+++ src/actions/index.js
@@ -6,8 +6,16 @@ export const fetchPosts = () => async dispatch => {
 	dispatch({ type: "FETCH_POSTS", payload: res.data });
 };
 
-export const fetchUser = id => async dispatch => {
+// export const fetchUser = id => async dispatch => {
+// 	const res = await jsonPlaceholder.get(`/users/${id}`);
+
+// 	dispatch({ type: "FETCH_USER", payload: res.data });
+// };
+
+export const fetchUser = id => dispatch => _fetchUser(id, dispatch);
+
+const _fetchUser = _.memoize(async (id, dispatch) => {
 	const res = await jsonPlaceholder.get(`/users/${id}`);
 
 	dispatch({ type: "FETCH_USER", payload: res.data });
-};
+});
